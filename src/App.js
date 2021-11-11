7f5f0a1745149f4fd291814a2bb3ef30c09dde66
abelcg <abelcg86@gmail.com>
2021-11-11 10:32:21 -0300

    se agrego el maquetado en app y se agrego el componente Frases

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-//import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/button';
+import Frases from './components/Frases'
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <section className='container my-5 d-flex flex-column align-items-center'>
+       <img src={logo} alt="logo de los simpsons" />
+       <Button variant='warning' className='my-5 px-1 w-50'>Obtener frase</Button>
+       <Frases></Frases>
+    </section>
   );
 }
 
---
+++ src/components/Frases.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Card  from "react-bootstrap/card";
+//import {Button, Card } from "react-bootstrap"
+
+const Frases = () => {
+  return (
+    <Card>
+      <Card.Body>
+        <div className="row">
+          <div className="col-12 col-md-4">
+              <img src="" alt="" />
+          </div>
+          <div className="col-12 col-md-8">
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Frases;
