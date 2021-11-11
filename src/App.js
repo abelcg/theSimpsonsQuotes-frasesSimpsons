001cfb0b5e5cc45d99246c7993108fca65023eaa
abelcg <abelcg86@gmail.com>
2021-11-11 11:35:52 -0300

    se agrego la consulta a la API

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import logo from './logo.png';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/button';
-import Frases from './components/Frases'
+import Frases from './components/Frases';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(() =>{
+    consultaApi();
+  }, [])
+
+  const consultaApi = async()=> {
+    const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+    const dato = await respuesta.json();
+    console.log(respuesta);
+    console.log(dato[0]);
+    // guardar en el state
+    setPersonaje(dato[0]);
+  }
+
   return (
     <section className='container my-5 d-flex flex-column align-items-center'>
        <img src={logo} alt="logo de los simpsons" />
