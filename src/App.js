4cd91a2e83166207bd6ad3056421afc0ae47905c
abelcg <abelcg86@gmail.com>
2021-11-20 22:14:50 -0300

    se agrego README

--- src/App.js
+++ src/App.js
@@ -36,13 +36,13 @@ function App() {
 
   return (
     <section className="container my-5 d-flex flex-column align-items-center">
-      <img src={logo} alt="logo de los simpsons" />
+      <img src={logo} alt="The Simpsons logo" />
       <Button
         variant="warning"
         className="my-5 px-1 w-50"
         onClick={() => consultaApi()}
       >
-        Obtener frase
+        Get Quotes
       </Button>
       {mostrarComponente}
     </section>
